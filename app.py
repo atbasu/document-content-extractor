14b9b1588297a38d799c0890a76e0bdb8ea4046f
sanjaykrmallick <59769859+sanjaykrmallick@users.noreply.github.com>
2023-05-30 20:47:21 +0530

    added app.py and extractor

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request, make_response
+from flask_jwt_extended import JWTManager
+from functools import wraps
+from document_content_extractor import *
+import time
+import jwt
+import asyncio
+import nest_asyncio
+nest_asyncio.apply()
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'closewise-secret'
+
+# Decorators
+def token_required(f):
+   @wraps(f)
+   def decorated(*args, **kwargs):
+       
+       token = None
+       if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split()[1]
+       if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+       try:
+          data = jwt.decode(token, app.config['JWT_SECRET_KEY'], algorithms=['HS256'])
+          request.token_data = data
+       except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired!'}), 401
+       except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token!'}), 401     
+       return f(*args, **kwargs)
+   return decorated
+
+async def extract_content(filePath):
+   return runApp(filePath)
+
+# Routes
+@app.route('/api/v1/multipart-parse', methods=['POST'])
+@token_required
+def get_users():
+    try:
+        try:
+            file = request.files['file']
+            if file:
+                timeString = time.strftime("%Y%m%d-%H%M%S")
+                print(f"file/{timeString}.pdf")
+                filePath = f"file/{timeString}.pdf"
+                file.save(filePath)
+                run_id, run_time, file_path, api_key, usage, model, json_result = asyncio.run(extract_content(filePath))
+                os.remove(filePath)
+                return json_result
+            
+        except Exception as e:
+            app.logger.error(str(e))
+            if e == "'choices'":
+                print("entereddddddddd")
+                run_id, run_time, file_path, api_key, usage, model, json_result = asyncio.run(extract_content(filePath))      
+                os.remove(filePath)
+                return json_result
+
+    except Exception as e:
+      app.logger.error(str(e))
+      errorMessage = "An error occurred while processing the request"
+      os.remove(filePath)
+      return jsonify({ 'error': errorMessage }), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
