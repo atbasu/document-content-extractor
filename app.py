1cfdacd5f6ed30bf7f849bde85b2ca0e5caaab97
Sanjay Kumar <sanjay@logic-square.com>
2023-06-02 00:04:31 +0530

    updated app.py for routes error

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import jwt
 import asyncio
 import nest_asyncio
 nest_asyncio.apply()
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'closewise-secret'
@@ -15,7 +16,7 @@ app.config['JWT_SECRET_KEY'] = 'closewise-secret'
 def token_required(f):
    @wraps(f)
    def decorated(*args, **kwargs):
-       
+
        token = None
        if 'Authorization' in request.headers:
             token = request.headers['Authorization'].split()[1]
@@ -27,42 +28,35 @@ def token_required(f):
        except jwt.ExpiredSignatureError:
             return jsonify({'message': 'Token has expired!'}), 401
        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Invalid token!'}), 401     
+            return jsonify({'message': 'Invalid token!'}), 401
        return f(*args, **kwargs)
    return decorated
 
 async def extract_content(filePath):
    return runApp(filePath)
 
-# Routes
+# Routeslk
 @app.route('/api/v1/multipart-parse', methods=['POST'])
 @token_required
 def get_users():
     try:
-        try:
-            file = request.files['file']
-            if file:
-                timeString = time.strftime("%Y%m%d-%H%M%S")
-                print(f"file/{timeString}.pdf")
-                filePath = f"file/{timeString}.pdf"
-                file.save(filePath)
-                run_id, run_time, file_path, api_key, usage, model, json_result = asyncio.run(extract_content(filePath))
-                os.remove(filePath)
-                return json_result
-            
-        except Exception as e:
-            app.logger.error(str(e))
-            if e == "'choices'":
-                print("entereddddddddd")
-                run_id, run_time, file_path, api_key, usage, model, json_result = asyncio.run(extract_content(filePath))      
-                os.remove(filePath)
-                return json_result
+       file = request.files['file']
+       if file:
+              timeString = time.strftime("%Y%m%d-%H%M%S")
+              print(f"file/{timeString}.pdf")
+              filePath = f"file/{timeString}.pdf"
+              file.save(filePath)
+              run_id, run_time, file_path, api_key, usage, model, json_result = asyncio.run(extract_content(filePath))
+              os.remove(filePath)
+              return json_result
 
     except Exception as e:
-      app.logger.error(str(e))
-      errorMessage = "An error occurred while processing the request"
-      os.remove(filePath)
-      return jsonify({ 'error': errorMessage }), 500
+       app.logger.error(str(e))
+       errorMessage = "An error occurred while processing the request"
+       os.remove(filePath)
+       return jsonify({ 'error': errorMessage }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    http_server = WSGIServer(('', 4500), app)
+    http_server.serve_forever()
