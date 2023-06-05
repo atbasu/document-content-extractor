9833ac4cc1389de49391c8fbcecdd2f93f6c8743
Sanjay Kumar <sanjay@logic-square.com>
2023-06-05 13:55:15 +0530

    updated app.py and closewise_parser

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
-import asyncio
+from flask import Flask, jsonify, request, make_response
+from flask_jwt_extended import JWTManager
 from functools import wraps
-
-import jwt
-import nest_asyncio
-from flask import Flask, jsonify, request
-
 from document_content_extractor import *
-
+import time
+import jwt
+import asyncio
+import nest_asyncio
 nest_asyncio.apply()
 from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'closewise-secret'
 
-
 # Decorators
 def token_required(f):
    @wraps(f)
    def decorated(*args, **kwargs):
-
+       
        token = None
        if 'Authorization' in request.headers:
             token = request.headers['Authorization'].split()[1]
        if not token:
             return jsonify({'message': 'Token is missing!'}), 401
        try:
-            data = jwt.decode(token, app.config['JWT_SECRET_KEY'], algorithms=['HS256'])
-            request.token_data = data
+          data = jwt.decode(token, app.config['JWT_SECRET_KEY'], algorithms=['HS256'])
+          request.token_data = data
        except jwt.ExpiredSignatureError:
             return jsonify({'message': 'Token has expired!'}), 401
        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Invalid token!'}), 401
+            return jsonify({'message': 'Invalid token!'}), 401     
        return f(*args, **kwargs)
    return decorated
 
+async def extract_content(filePath):
+   return run_app(filePath)
 
 # Routeslk
 @app.route('/api/v1/multipart-parse', methods=['POST'])
