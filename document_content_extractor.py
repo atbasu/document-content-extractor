cc31bbaaa2461456ff005bc71ec238bc7bcbe766
Sanjay Kumar <sanjay@logic-square.com>
2023-07-11 15:57:37 +0530

    merge and resove

--- document_content_extractor.py
+++ document_content_extractor.py
@@ -124,7 +124,7 @@ def generate_correction_prompt(corrections, result):
     returned_values = ''
     correct_values = ''
     for key, value in corrections.items():
-        returned_values += f"{key} : {result[key]}\n"
+        returned_values += f"{key} : {result['data'][key]}\n"
         correct_values += f"{key} : {value['correct_value']}\n{value['justification']}\n"
     correction_prompt = f"I am using the open AI completion api to to process the following prompt:\n" \
                         f"----------------------\n{prompt}\n----------------------\nHowever, the value" \
@@ -157,16 +157,17 @@ def check_for_errors(result, logger=None):
         # If the user says values are incorrect
         elif user_input.lower() == 'y':
             # Ask the user for the field name that needs to be corrected
+            data = result['data']
             field_names = input('Enter a coma separated list of fields that need to be corrected? ').split(',')
             # Check if the field name is valid
             for field_name in field_names:
                 field_name = field_name.strip()
-                if field_name not in result.keys():
+                if field_name not in data.keys():
                     print(f'{field_name} is an invalid field_name. Please enter a valid field name.')
                     continue
                 # Ask the user for the correct value
                 correct_value = input(f"What should the correct value be for {field_name}?")
-                if correct_value == result[field_name]:
+                if correct_value == data[field_name]:
                     print('This value is the same as what was extracted, moving on to the next one.')
                     continue
                 # Ask the user for a justification
